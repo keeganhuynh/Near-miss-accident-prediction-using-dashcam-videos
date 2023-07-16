e4fde05c967a7694606244255c542f8a20ae3f10
Huỳnh Hoàng Khánh <crisstanhuynh382@gmail.com>
2023-07-16 19:52:08 +0700

    Delete

--- README.md
+++
@@ -1,3 +0,0 @@
-# Near-miss-accident-prediction-using-dashcam-video
-
-IF YOU HAVE A TROUBLE WHEN RUNING RUN.PY  "Deephough is not found", JUST RUN THIS FILE AGAIN
--- setup.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-
-def SettingEnvironment():
-    #set_up_for_DEEPHOUGH
-    directory_path = 'DeepHough/model/_cdht/'
-    subprocess.call(["python", "setup.py", "install"], cwd=directory_path)
